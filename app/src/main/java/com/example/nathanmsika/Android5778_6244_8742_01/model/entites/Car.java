dcb300b38ffef4510f420cbde41050245766a0b2
hillelchitrit <hillelchitrit97@gmail.com>
2017-11-07 14:54:52 +0200

    Update Car.java

--- app/src/main/java/com/example/nathanmsika/Android5778_6244_8742_01/model/entites/Car.java
+++ app/src/main/java/com/example/nathanmsika/Android5778_6244_8742_01/model/entites/Car.java
@@ -4,7 +4,7 @@ package com.example.nathanmsika.Android5778_6244_8742_01.model.entites;
  * Created by samuel on 02-Nov-17.
  */
 public class Car {
-    protected int branchParkNumber;
+    protected int branchParkNumbe;
     protected float km;
     protected String carNumber;
     protected char _class;
